d2d9ae43866ce04ae38e24476253bbc9f968acfd
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2021-05-14 20:42:58 +0200

    Fix Update

--- VitalTools/VitalTools/Api/ApiRequester.cs
+++ VitalTools/VitalTools/Api/ApiRequester.cs
@@ -1,11 +1,12 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace VitalTools.Api
 {
@@ -38,29 +39,29 @@ namespace VitalTools.Api
                 message.EnsureSuccessStatusCode();
                 string json = message.Content.ReadAsStringAsync().Result;
 
-                return JsonConvert.DeserializeObject<TResult>(json);
+				return JsonConvert.DeserializeObject<TResult>(json);
             }
         }
 
         public void Add<TBody>(TBody body, string uri)
         {
-            string json = JsonConvert.SerializeObject(body);
-            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage message = _client.PostAsync(uri, content).Result;
-            
-            if (!message.IsSuccessStatusCode)
-                throw new HttpRequestException();
-        }
+			string json = JsonConvert.SerializeObject(body);
+			HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+			HttpResponseMessage message = _client.PostAsync(uri, content).Result;
+
+			if (!message.IsSuccessStatusCode)
+				throw new HttpRequestException();
+		}
 
         public void Edit<TBody>(TBody body, string uri)
         {
-            string json = JsonConvert.SerializeObject(body);
-            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage message = _client.PutAsync(uri, content).Result;
+			string json = JsonConvert.SerializeObject(body);
+			HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+			HttpResponseMessage message = _client.PutAsync(uri, content).Result;
 
-            if (!message.IsSuccessStatusCode)
-                throw new HttpRequestException();
-        }
+			if (!message.IsSuccessStatusCode)
+				throw new HttpRequestException();
+		}
 
         public void Delete(int id, string uri)
         {
